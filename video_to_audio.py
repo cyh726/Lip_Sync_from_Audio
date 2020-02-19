36cf9de2aebf4633010cfa577473902c1c7e06b5
cyh726 <cyhleo726@gmail.com>
2020-02-19 21:18:35 +0800

    Upload the project

---
+++ video_to_audio.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': '1.mp3',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
